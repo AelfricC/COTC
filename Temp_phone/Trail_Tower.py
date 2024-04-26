b2b116bd439c65fc580b9ec0b3e8ccac9db19f74
AelfricC <150880691+AelfricC@users.noreply.github.com>
2024-04-26 13:42:11 +0930

    Update Trail_Tower.py

--- Temp_phone/Trail_Tower.py
+++ Temp_phone/Trail_Tower.py
@@ -7,9 +7,8 @@ from Phone_COTC import *
 ui = xp.ui()
 tower_name = ui.stringValue("tower_name")
 
-battle = ["#F5F6F6", 1880, 953]
-battle_xy = [1880, 953]
-battle_confirm = ["#FEFEFE", 1392, 721]
+
+
 def Master():
     count = 0
     print("start")
@@ -19,28 +18,18 @@ def Master():
     skills_array3 = [[2,2], [-4,2], [4,2], [1]]
     skills_array4 = [[0], [0], [2], [0]]
     while True:
-        tap_After_checking(battle,-1)
-        tap_After_checking(battle_confirm,-1)
-        wait_Battle()
+        tap_After_checking(battle_icon_InTower,-1)
+        tap_After_checking(battle_icon_InTower_confirm,-1)
         select_char_and_skill(characters_array, skills_array1)
-        only_atk()
-        wait_Battle()
         select_char_and_skill(characters_array, skills_array2)
-        only_atk()
-        wait_Battle()
         select_char_and_skill(characters_array, skills_array3)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array4)
-        boost_Atk()
-        time.sleep(10)
+        select_char_and_skill(characters_array, skills_array4,2,1)
         count += 1
-        tap_Until_Exsit(battle,battle_xy)
+        tap_Until_Exsit(battle_icon_InTower,battle_icon_InTower_xy)
         time.sleep(4)
         xp.toast("this is " + str(count))
 
 def Elite_sword():
-    count = 0
     print("start")
     characters_array = [1, 2, 3, 4]
     skills_array1 = [[2,3], [4], [3,3], [-4,3]]
@@ -63,76 +52,43 @@ def Elite_sword():
     skills_array18 = [[1], [3,2], [4,3], [2,2]]
     skills_array19 = [[4], [14], [0], [100]]
     skills_array20 = [[1], [13,3], [4,3], [2,3]]
-    while True:
-        tap_After_checking(battle,-1)
-        tap_After_checking(battle_confirm,-1)
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array1)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array2)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array3)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array4)
-        only_atk()
-        select_char_and_skill(characters_array, skills_array5)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array6)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array7)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array8)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array9)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array10)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array11)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array12)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array13)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array14)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array15)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array16)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array17)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array18)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array19)
-        only_atk()
-        wait_Battle()
-        select_char_and_skill(characters_array, skills_array20)
-        only_atk()
-        time.sleep(10)
-        count += 1
-        tap_Until_Exsit(battle,battle_xy)
-        time.sleep(4)
-        xp.toast("this is " + str(count))
+tap_After_checking(battle_icon_InTower,-1)
+tap_After_checking(battle_icon_InTower_confirm,-1)
+wait_Battle()
+select_char_and_skill(characters_array, skills_array1)
+select_char_and_skill(characters_array, skills_array2)
+select_char_and_skill(characters_array, skills_array3)
+select_char_and_skill(characters_array, skills_array4)
+select_char_and_skill(characters_array, skills_array5)
+select_char_and_skill(characters_array, skills_array6)
+select_char_and_skill(characters_array, skills_array7)
+select_char_and_skill(characters_array, skills_array8)
+select_char_and_skill(characters_array, skills_array9)
+select_char_and_skill(characters_array, skills_array10)
+select_char_and_skill(characters_array, skills_array11)
+select_char_and_skill(characters_array, skills_array12)
+select_char_and_skill(characters_array, skills_array13)
+select_char_and_skill(characters_array, skills_array14)
+select_char_and_skill(characters_array, skills_array15)
+select_char_and_skill(characters_array, skills_array16)
+select_char_and_skill(characters_array, skills_array17)
+select_char_and_skill(characters_array, skills_array18)
+select_char_and_skill(characters_array, skills_array19)
+select_char_and_skill(characters_array, skills_array20,1,1)
+count += 1
+tap_Until_Exsit(battle_icon_InTower,battle_icon_InTower_xy)
+time.sleep(4)
+xp.toast("this is " + str(count))
+
+def Elite_fire():
+    print("start")
+
+
+   
 
 if tower_name == "Master":
     Master()
 elif tower_name == "Elite_sword":
     Elite_sword()
-
+elif tower_name == "Elite_fire":
+    Elite_fire()
